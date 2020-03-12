24ed56fe4159e96f81b19ee901914e74a69677cc
Alexey Golenko <44725291+Deker39@users.noreply.github.com>
2020-03-12 22:37:40 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-from core import Information, colors
+import Information, colors
 import os, time, subprocess
 from sys import stdout
 
@@ -16,31 +16,31 @@ def Command_exe(msg, cmd):
 
 print ( "" )
 print (
-    "                    "+colors.B+",:"+colors.W+"       "+colors.B+":,"+colors.W+"                   				 " )  # ,:       :,
+    "                    "+colors.B+",:"+colors.W+"       "+colors.B+":,"+colors.W+"                                 " )  # ,:       :,
 print (
-    "                   "+colors.B+",/./"+colors.W+" _8_  "+colors.B+"\.\                  				 " )  # /./  _8_  \.\
+    "                   "+colors.B+",/./"+colors.W+" _8_  "+colors.B+"\.\                                " )  # /./  _8_  \.\
 print (
-    "                   "+colors.B+",\ \\"+colors.W+"/"+colors.P+"("+colors.R+"O"+colors.P+")"+colors.W+"\\"+colors.B+"/ /                  				 " )  # \ \/( O )\/ /
+    "                   "+colors.B+",\ \\"+colors.W+"/"+colors.P+"("+colors.R+"O"+colors.P+")"+colors.W+"\\"+colors.B+"/ /                                   " )  # \ \/( O )\/ /
 print (
-    "                    "+colors.B+",\ \:::/ /                   				 ")#                    \ \:::::/ /
+    "                    "+colors.B+",\ \:::/ /                                  ")#                    \ \:::::/ /
 print (
-    "                     /"+colors.O+"__"+colors.G+"---"+colors.O+"__"+colors.B+"\                   				 "  )#                     /__---__\
+    "                     /"+colors.O+"__"+colors.G+"---"+colors.O+"__"+colors.B+"\                                  "  )#                     /__---__\
 print (
-    "                    ("+colors.O+"/__\ /__\\"+colors.B+")"+colors.W+"                   				 ")#                    (/__\ /__\)
+    "                    ("+colors.O+"/__\ /__\\"+colors.B+")"+colors.W+"                                    ")#                    (/__\ /__\)
 print (
-    "                    "+colors.B+"/"+colors.W+"\  .V.  /"+colors.B+"\\"+colors.W+"                   				 ")#                    /\  .V.  /\
+    "                    "+colors.B+"/"+colors.W+"\  .V.  /"+colors.B+"\\"+colors.W+"                                    ")#                    /\  .V.  /\
 print (
-    "                   "+colors.B+"/"+colors.W+"  \,---,/  "+colors.B+"\\"+colors.W+"                  				 ")#                   /  \,---,/  \
+    "                   "+colors.B+"/"+colors.W+"  \,---,/  "+colors.B+"\\"+colors.W+"                                   ")#                   /  \,---,/  \
 print (
-    "                   "+colors.B+"\\"+colors.W+"___TTTTT___"+colors.B+"/"+colors.W+"         CODE: KATANA  ")#                   \___TTTTT___/
+    "                   "+colors.B+"\\"+colors.W+"___TTTTT___"+colors.B+"/"+colors.W+"         CODE: MIRROR  ")#                   \___TTTTT___/
 print ("                ::::\ "+colors.R+"|"+colors.W+"_____"+colors.R+"|"+colors.W+" /::::      DATE: "+Information.date+"")#                ::::\ "+colors.R+"|"+colors.W+"_____"+colors.R+"|"+colors.W+" /::::
 print ("                (+  _"+colors.R+"|"+colors.W+" __"+colors.R+"|"+colors.W+"__ "+colors.R+"|"+colors.W+"_  +)      CORE: "+Information.version+", BUILD: "+Information.build )#                (+  _"+colors.R+"|"+colors.W+" __"+colors.R+"|"+colors.W+"__ "+colors.R+"|"+colors.W+"_  +)
-print ("        "+Information.Type+"    "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"KATANA."+colors.R+"|"+colors.W+"_I  "+colors.R+"|"+colors.W+"                               ")#                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+".ANATAK"+colors.R+"|"+colors.W+"_I  "+colors.R+"|"+colors.W+"
-print ("                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"  "+colors.R+"|"+colors.W+"               				 ")#                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"  "+colors.R+"|"+colors.W+"
-print ("    __________?_____________________________________  				 ")#    __________?_________________________________
-print ("   {_"+colors.C+"A|"+colors.W+" "+colors.C+"L|"+colors.W+" "+colors.R+"E|"+colors.W+" "+colors.W+"X|"+colors.W+" |"+colors.GR+"#################################"+colors.W+"/  				 ")#   {_"+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" I#################################/
-print ("     ^ ^ ^ ^   ,ww   "+colors.O+"FRAMEWORK"+colors.W+"   ww,                   				 ")#     ^ ^ ^ ^ EHT KROWEMARF, dliuB
-print ("                   I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_I                  				 ")#                   I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_I
-print ("                   \_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_/                  				 ")#                   \_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_/
+print ("        "+Information.Type+"    "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"GOLENKO"+colors.R+"|"+colors.W+"_I  "+colors.R+"|"+colors.W+"                               ")#                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+".ANATAK"+colors.R+"|"+colors.W+"_I  "+colors.R+"|"+colors.W+"
+print ("                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"  "+colors.R+"|"+colors.W+"                              ")#                "+colors.R+"|"+colors.W+"  I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"  "+colors.R+"|"+colors.W+"
+print ("    __________?_____________________________________                 ")#    __________?_________________________________
+print ("   {_"+colors.C+"A|"+colors.W+" "+colors.C+"L|"+colors.W+" "+colors.R+"E|"+colors.W+" "+colors.W+"X|"+colors.W+" |"+colors.GR+"#################################"+colors.W+"/                ")#   {_"+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" "+colors.R+"|"+colors.W+" I#################################/
+print ("     ^ ^ ^ ^   ,ww   "+colors.O+".PROJECT."+colors.W+"   ww,                                 ")#     ^ ^ ^ ^ EHT KROWEMARF, dliuB
+print ("                   I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_I                                 ")#                   I_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_I
+print ("                   \_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_/                                 ")#                   \_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_"+colors.R+"|"+colors.W+"_/
 print ("")
 print ("")
