0664c08c8b543461eb237e47d05c7b2552f9125f
deker39 <bigtigerlesha@gmail.com>
2020-03-29 14:05:07 +0300

    examole

---
+++ face_example.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+import os
+import subprocess
+
+import time
+from motion_sensor import get_sensor
+from cffi.setuptools_ext import execfile
+
+recognizer = cv2.face.LBPHFaceRecognizer_create ( )
+recognizer.read ( 'trainer/trainer.yml' )
+cascadePath = "/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier ( cascadePath );
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+program = " python /home/pi/dip/main.py"
+program1 = "sudo python /home/pi/dip/backlight.py -c"
+# iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'Alexey']
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture ( 0 )
+cam.set ( 3, 640 )  # set video widht
+cam.set ( 4, 480 )  # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get ( 3 )
+minH = 0.1 * cam.get ( 4 )
+
+while True :
+    if get_sensor ( ) :
+        # subprocess.Popen(program1 , shell = True)
+        ret, img = cam.read ( )
+        img = cv2.flip ( img, -1 )  # Flip vertically
+        gray = cv2.cvtColor ( img, cv2.COLOR_BGR2GRAY )
+
+        faces = faceCascade.detectMultiScale (
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int ( minW ), int ( minH )),
+        )
+
+        for (x, y, w, h) in faces :
+            cv2.rectangle ( img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
+            id, confidence = recognizer.predict ( gray[y :y + h, x :x + w] )
+
+            # Check if confidence is less them 100 ==> "0" is perfect match
+            if (confidence < 100) :
+                kek = confidence
+                id = names[id]
+                confidence = "  {0}%".format ( round ( 100 - confidence ) )
+                if (kek > 80) :
+                    # execfile("main.py", globals())
+                    process = subprocess.Popen ( program, shell=True )
+                    code = process.wait ( )
+            else :
+                id = "unknown"
+                confidence = "  {0}%".format ( round ( 100 - confidence ) )
+
+            cv2.putText ( img, str ( id ), (x + 5, y - 5), font, 1, (255, 255, 255), 2 )
+            cv2.putText ( img, str ( confidence ), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1 )
+
+        cv2.imshow ( 'camera', img )
+
+        k = cv2.waitKey ( 10 ) & 0xff  # Press 'ESC' for exiting video
+        if k == 27 :
+            break
+        # time.sleep(10)
+
+    else :
+        # print("Not found")
+        # cam.release()
+        cv2.destroyAllWindows ( )
+
+# Do a bit of cleanup
+print ( "\n [INFO] Exiting Program and cleanup stuff" )
+cam.release ( )
+cv2.destroyAllWindows ( )
+
+
+
