2937f187f1052a004268d843564dad1bc5c122cc
deker39 <bigtigerlesha@gmail.com>
2020-04-05 18:35:13 +0300

    сделанные функции для роботы

--- db.py
+++ db.py
@@ -14,40 +14,42 @@ def call_cursor(con):
     cur = con.cursor ( )
     return cur
 
-def creat_table_db(cur,con):
-    cur.execute ( '''CREATE TABLE USER_PHOTO
+def creat_table_db(cur,con,table_name):
+    cur.execute ( '''CREATE TABLE %s
          (ID INT PRIMARY KEY NOT NULL,
          NAME TEXT NOT NULL,
          AGE INT NOT NULL,
          MAIL CHAR(50),
          COUNTRY CHAR(50),
-         LANGUAGE CHAR(50));''' )
+         LANGUAGE CHAR(50));''' % (table_name,) )
     con.commit ( )
     con.close ( )
     print ( "Table created successfully" )
 
-def select_db(cur):
-    cur.execute ( "SELECT id,name,age,mail,country, language from USER_NAME" )
+
+#def value():
+
+def select_db(cur,table_name,values):
+    cur.execute ( "SELECT %s from %s" %(values,table_name,))
     rows = cur.fetchall ( )
     return rows
 
-def insert_db(cur,con):
+def insert_db(cur,con,table_name,title,values):
     cur.execute (
-        "INSERT INTO USER_NAME (ID,NAME,AGE,MAIL,COUNTRY,LANGUAGE) VALUES (3, 'John', 18,"
-        " 'bigtige@gmial.com', 'Ukrain','ukrain')" )
+        "INSERT INTO %s (%s) VALUES %s" %(table_name,title,values,))
     con.commit()
     con.close ( )
     print("Record inserted successfully into table")
 
 
-def delete_db(cur,con,table_name,pk,number):
-    cur.execute ( "DELETE from %s where %s=%s" %(table_name,pk,number,) )
+def delete_db(cur,con,table_name,pk,number_pk):
+    cur.execute ( "DELETE from %s where %s=%s" %(table_name,pk,number_pk,) )
     con.commit ( )
     con.close ( )
     print ( "Record deleted successfully" )
 
-def update_db(cur,con):
-    cur.execute ( "UPDATE USER_NAME set AGE = 25 where ID = 2" )
+def update_db(cur,con,table_name,values,number_values,pk,number_pk):
+    cur.execute ( "UPDATE %s set %s = %s where %s =%s" %(table_name,values,number_values,pk,number_pk,) )
     con.commit ( )
     con.close ( )
     print ( "Record updated successfully" )
--- face_example.py
+++ face_example.py
@@ -1,15 +1,18 @@
 import db
 
+table_name='USER_NAME'
+pk ='ID'
+number_pk = 2
+values = 'id,name,age,mail,country, language'
+
 names = []
 con = db.call_db()  #вызов бв
 cur = db.call_cursor(con)#вызов курсора
-rows = db.select_db(cur)#выборка таблицы
+rows = db.select_db(cur,table_name,values)#выборка таблицы
 
 for row in rows:
     print(row [:][1])
-table_name='USER_NAME'
-pk ='ID'
-number = 2
-#db.delete_db(cur,con,table_name,pk,3)
-db.delete_db(cur,con,table_name,pk,number)
+#title  = 'ID,NAME,AGE,MAIL,COUNTRY,LANGUAGE'
+#values = (7, 'Marina', 21, 'shluha@gmial.com', 'Russian','ukrain')
 
+#db.insert_db(cur,con,table_name,title,values)
